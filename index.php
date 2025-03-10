9cbb42065b07bdcbfef63fb4aed88c962c1e2b43
Rikesh Bajracharya <rikesh.bajracharya@gmail.com>
2025-03-10 10:33:43 +0545

    refactor: cleanup

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
 <?php
 
-require_once 'User.php';
-require_once 'Role.php';
-
-$user = new User();
-// echo $user->index();
-
-$role = new Role("Rupesh");
-echo Role::getManagementName();
-
-// echo $role->getManagementName();
-
-
-
-
-// echo $role->getRole();
-// echo $role->index();
-
-
-
-
+print("hello world");
