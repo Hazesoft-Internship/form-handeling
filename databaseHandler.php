f986e72574ee8975661b53b73abb3a05f0305c3f
@Samhubby <samayaasylumml@gmail.com>
2025-03-10 14:30:11 +0545

    feat: Enhance input validation in sanitizeUserInput method

--- databaseHandler.php
+++ databaseHandler.php
@@ -3,8 +3,15 @@ require_once 'dbConnection.php';
 
 class ValidateInput
 {
-    protected function sanitizeUserInput($firstName, $middleName, $lastName, $email, $address): array
+    protected function sanitizeUserInput($firstName, $middleName, $lastName, $email, $address): array | string
     {
+
+        $patternName = "/^[a-zA-Z0-9]+$/";
+        $patternEmail = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+
+        if (!preg_match($patternName, $firstName) || !preg_match($patternName, $middleName) || !preg_match($patternName, $lastName) || !preg_match($patternEmail, $email)) {
+            return "Invalid input";
+        }
         $userArr = [$firstName, $middleName, $lastName, $email, $address];
         $sanitizedArr = [];
         $sanitizedArr = array_map(function ($userData) {
@@ -37,6 +44,10 @@ class DatabaseHandler extends ValidateInput
             $address = $_POST["address"];
 
             $sanitizedArr = $this->sanitizeUserInput($firstName, $middleName, $lastName, $email, $address);
+            if ($sanitizedArr === "Invalid input") {
+                echo "Invalid input";
+                return;
+            }
 
             $insertQuery = "INSERT INTO users (firstName, middleName, lastName, email, address) VALUES ('$sanitizedArr[0]','$sanitizedArr[1]','$sanitizedArr[2]','$sanitizedArr[3]','$sanitizedArr[4]')";
             $insertResult = mysqli_query($this->result, $insertQuery);
