fe00e999bf8c48707df5d5553d13a1f5e84b7053
Suyash <suyash.shraddha52@gmail.com>
2025-03-10 07:25:14 +0000

    feat: add entire project directory

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6124e2b (feat: add entire project directory)
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,6 +30,7 @@
     <input type="submit" value="Submit">
 </form>
 
+<<<<<<< HEAD
 <?php
 require 'classes/DB.php';
 require 'classes/User.php';
@@ -89,26 +93,70 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </body>
 </html>
 =======
+=======
+>>>>>>> 6124e2b (feat: add entire project directory)
 <?php
+require 'classes/DB.php';
+require 'classes/User.php';
 
-require_once 'User.php';
-require_once 'Role.php';
-
-$user = new User();
-// echo $user->index();
-
-$role = new Role("Rupesh");
-echo Role::getManagementName();
-
-// echo $role->getManagementName();
+use Classes\DB;
+use Classes\User;
 
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    // Form Validation 
+    if (isset($_POST['first_name'], $_POST['last_name'], $_POST['address'], $_POST['email']) &&
+        !empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['address']) && !empty($_POST['email'])) {
+        
+        $db = new DB();
+        $user = new User($db);
+        $conn = $db->getConnection();
+        
+        // Preventing unusual input
+        $userData = [
+            'first_name' => htmlspecialchars($_POST['first_name']),
+            'middle_name' => isset($_POST['middle_name']) ? htmlspecialchars($_POST['middle_name']) : '', 
+            'last_name' => htmlspecialchars($_POST['last_name']),
+            'address' => htmlspecialchars($_POST['address']),
+            'email' => htmlspecialchars($_POST['email'])
+        ];
 
+        // Inserting data into database
+        if ($user->insertUser($userData)) {
+            echo "User information added into database successfully. <br>";
+            
+            // Exporting data to CSV
+            $sql = "SELECT CONCAT(first_name, ' ', IFNULL(middle_name, ''), ' ', last_name) AS full_name, address, email FROM users";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// echo $role->getRole();
-// echo $role->index();
+            $csvFile = 'info.csv';
+            $file = fopen($csvFile, 'w');   
 
+            fputcsv($file, ['Full Name', 'Address', 'Email']); // CSV Headers
 
+            foreach ($users as $user) {
+                fputcsv($file, $user);
+            }
 
+            fclose($file);
 
+<<<<<<< HEAD
 >>>>>>> 40cef57 (feat: added inital phase requirement)
+=======
+            echo "User information exported successfully to $csvFile.";
+        } else {
+            echo "Error adding user.";
+        }
+    // Error if the form is not submitted properly
+    } else {
+        echo "Form Submission Denied. <br> Kindly enter all *Required* fields (First Name, Last Name, Address, Email).";
+    }
+}
+?>
+
+</body>
+</html>
+>>>>>>> 6124e2b (feat: add entire project directory)
