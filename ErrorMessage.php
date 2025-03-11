560a25946e123864d617fbe4f0de0447f5cb2dfa
Ayush Tamang <ayushtamang1995@gmail.com>
2025-03-11 12:23:42 +0545

    fix: error message correction middle name and address

--- ErrorMessage.php
+++ ErrorMessage.php
@@ -4,11 +4,11 @@ class ErrorMessage {
 
     public static $firstNameCharacters = "Your first name must be between 2 and 25 char.";
     
-    public static $middleNameCharacters = "Your first name must be between 2 and 25 char.";
+    public static $middleNameCharacters = "Your middle name must be between 2 and 25 char.";
     
     public static $lastNameCharacters = "Your last name must be between 2 and 25 char.";
     
-    public static $addressCharacters = "Your last name must be between 2 and 25 char.";
+    public static $addressCharacters = "Your address must be between 2 and 25 char.";
     
     public static $emailInvalid = "Invalid email.";
 }
